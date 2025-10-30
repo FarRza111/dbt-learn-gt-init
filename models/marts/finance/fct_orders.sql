580fe01d9688366a93b639a7d1468f7c18442a04
FarRza111 <farrza111@gmail.com>
2025-10-30 22:32:30 +0200

    init

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -1,32 +1,32 @@
 
-with orders as (
-    select * from {{ ref('stg_jaffle_shop__orders') }}
-),
+-- with orders as (
+--     select * from {{ ref('stg_jaffle_shop__orders') }}
+-- ),
 
-payments as (
-    select * from {{ ref('stg_stripe__payment') }} 
-),
-
-
-order_payments as (
-    select 
-        order_id,
-        sum(case when status = 'success' then amount end) as amount 
-    from payments 
-    group by order_id
-),
+-- payments as (
+--     select * from {{ ref('stg_stripe__payment') }} 
+-- ),
 
 
-final as (
+-- order_payments as (
+--     select 
+--         order_id,
+--         sum(case when status = 'success' then amount end) as amount 
+--     from payments 
+--     group by order_id
+-- ),
 
-    select 
-        orders.order_id,
-        orders.customer_id,
-        orders.order_date
-    from orders
-    left join order_payments using (order_id)
+
+-- final as (
+
+--     select 
+--         orders.order_id,
+--         orders.customer_id,
+--         orders.order_date
+--     from orders
+--     left join order_payments using (order_id)
 
 
-)
+-- )
 
-select * from final
+-- select * from final
--- models/staging/jaffle_shop/stripe/stg_stripe__payment.sql
+++ models/staging/jaffle_shop/stripe/stg_stripe__payment.sql
@@ -1,9 +1,26 @@
-select
-    id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
-    amount/100 as amount,
-    cast(created as timestamp) as created_at
+-- select
+--     id as payment_id,
+--     orderid as order_id,
+--     paymentmethod as payment_method,
+--     status,
+--     amount/100 as amount,
+--     cast(created as timestamp) as created_at
 
-from {{ source('stripe', 'payment') }}
+-- from {{ source('stripe', 'payment') }}
+
+
+with source as (
+    select * from {{ source('stripe', 'payment') }}
+),
+    transformed as (
+    select 
+        id as payment_id,
+        orderid as order_id,
+        created as payment_created_at,
+        status as payment_status,
+        round(amount/100.0, 2) as payment_amount
+    from source
+    
+    )
+
+select * from transformed
