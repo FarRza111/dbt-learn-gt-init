93c9b51171c7d8314d7e2934b1dfbb6d20ce7d11
FarRza111 <farrza111@gmail.com>
2025-11-03 00:15:39 +0200

    jinja approach added

--- analysis/jinja_practice.sql
+++ analysis/jinja_practice.sql
@@ -1,20 +1,25 @@
-WITH 
-    payment 
-AS (
-    select 
-    * 
-    from {{ ref('stg_stripe__payment') }}
-    where 
-        payment_status = 'success'
-)
-, pivoted AS 
-(
-    select 
-        order_id
-        , sum(case when payment_method = 'bank_transfer' then payment_amount else 0 end) as bank_transfer_amount
-    from payment
-    group by order_id
-)
+with 
+    payment as (
+        select 
+            * 
+        from {{ ref('stg_stripe__payment') }}
+        where 
+            payment_status = 'success'
+    ),
 
+    pivoted as (
+        select
+            order_id,
+            
+            {% set payment_methods = ['credit_card', 'bank_transfer'] %}
+            
+            {% for payment_method in payment_methods %}
+                sum(case when payment_method = '{{ payment_method }}' then payment_amount else 0 end) as {{ payment_method }}_amount
+                {%- if not loop.last -%},{%- endif %}
+            {% endfor %}
+
+        from payment
+        group by 1
+    )
 
 select * from pivoted
